f8b89d44dd40446e099da458cf45b5bda7761bba
April Hall <ari@arithefirst.com>
2024-10-14 12:07:57 -0400

    Add mobile frendliness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,10 @@
 
 </script>
 
-<form method="post" use:enhance enctype="multipart/form-data" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit">
-    <input required type="password" name="password" placeholder="password" class="input input-bordered mb-2 w-full max-w-xs" /><br>
-    <input required type="file" name="file" class="file-input file-input-bordered w-full max-w-xs"/><br>
-    <button class="btn-block btn max-w-xs mt-2" type="submit">Submit</button><br>
+<form method="post" use:enhance enctype="multipart/form-data" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-fit">
+    <input required type="password" name="password" placeholder="password" class="input input-bordered mb-2 w-full md:max-w-xs" /><br>
+    <input required type="file" name="file" class="file-input file-input-bordered w-full md:max-w-xs"/><br>
+    <button class="btn-block btn md:max-w-xs mt-2" type="submit">Submit</button><br>
     {#if wrongPassword}
         <p class="text-error text-center">Invalid Password</p>
     {/if}
