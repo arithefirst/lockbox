2ff6c8cf7444d792c3c2f7c1cff572b864de5252
April Hall <ari@arithefirst.com>
2024-10-14 11:48:55 -0400

    Add error handling for incorrect passwords

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
-<script>
+<script lang="ts">
     import {enhance} from "$app/forms";
+    import {page} from "$app/stores";
+    let wrongPassword: boolean = false;
+
+    function onSubmit() {
+        if ($page.status === 401) {
+            console.error("Invalid password entered.")
+            wrongPassword = true;
+        }
+    }
 </script>
 
 <form method="post" use:enhance enctype="multipart/form-data" class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit">
     <input required type="password" name="password" placeholder="password" class="input input-bordered mb-2 w-full max-w-xs" /><br>
     <input required type="file" name="file" class="file-input file-input-bordered w-full max-w-xs"/><br>
-    <button class="btn-block btn max-w-xs mt-2" type="submit">Submit</button><br>
+    <button class="btn-block btn max-w-xs mt-2" type="submit" on:click={onSubmit}>Submit</button><br>
+    {#if wrongPassword}
+        <p class="text-error text-center">Invalid Password</p>
+    {/if}
 </form>
+
