cf6ca4b93ca190900d1438faf8480a9af663060c
April Hall <ari@arithefirst.com>
2024-10-15 12:53:31 -0400

    Move makeId function to makeid.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,18 +1,7 @@
 import { fail } from '@sveltejs/kit';
 import sql from '$lib/SQL'
+import makeId from '$lib/makeid'
 import { writeFileSync, existsSync } from 'fs';
-
-function makeId(length: number) {
-    let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
-    while (counter < length) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        counter += 1;
-    }
-    return result;
-}
 
 export const actions = {
     default: async ({ request }) => {
