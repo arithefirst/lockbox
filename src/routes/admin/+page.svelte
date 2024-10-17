51c21624ba2106e8d159871397fae62ec81e1a5f
April Hall <ari@arithefirst.com>
2024-10-17 14:28:00 -0400

    Add empty squares for later use

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -2,10 +2,32 @@
     import DataTable from "\$lib/data-table.svelte";
 </script>
 
-<div class="w-screen">
-    <div class="w-full md:w-1/2 h-screen p-6 relative">
+<div class="w-screen h-screen relative flex">
+    <div class="w-full md:w-1/2 h-screen md:pl-6 md:py-6 md:pr-3 relative">
         <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
             <DataTable />
+        </div>
+    </div>
+    <div class="grid grid-cols-2 w-1/2">
+        <!-- Top-Left -->
+        <div class="w-full md:px-3 md:pt-6 md:pb-3 relative">
+            <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
+            </div>
+        </div>
+        <!-- Top-Right -->
+        <div class="w-full md:pl-3 md:pr-6 md:pt-6 md:pb-3 relative">
+            <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
+            </div>
+        </div>
+        <!-- Bottom-Left -->
+        <div class="w-full md:px-3 md:pt-3 md:pb-6 relative">
+            <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
+            </div>
+        </div>
+        <!-- Bottom-Right -->
+        <div class="w-full md:pl-3 md:pr-6 md:pt-3 md:pb-6 relative">
+            <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
+            </div>
         </div>
     </div>
 </div>
