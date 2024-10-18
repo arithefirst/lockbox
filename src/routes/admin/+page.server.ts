191190dd028c9dd04fb42c75aaf1de05b1fdaa8f
April Hall <ari@arithefirst.com>
2024-10-18 10:45:32 -0400

    Implement upload password function

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,4 +1,5 @@
 import isAdmin from "$lib/admin-verify.js";
+import sql from '$lib/SQL'
 import {redirect} from "@sveltejs/kit";
 
 export async function load( {cookies} ) {
@@ -8,3 +9,17 @@ export async function load( {cookies} ) {
         redirect(307, "/admin/login")
     }
 }
+
+export const actions = {
+    addPassword: async ({request}) => {
+        const formData = Object.fromEntries(await request.formData());
+
+        const password = formData.password as string;
+        const maxuses = formData.maxuses as string;
+
+        console.log(maxuses, +maxuses, password);
+
+        const insert = await sql`INSERT INTO passwords (password, max_uses, times_used, date)
+        VALUES (${password}, ${+maxuses}, 0, ${Math.floor(Date.now() / 1000)})`
+    },
+};
--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,31 +1,40 @@
 <script lang="ts">
     import DataTable from "\$lib/data-table.svelte";
+    import {page} from "$app/stores";
+    import {enhance} from "$app/forms";
 </script>
 
-<div class="w-screen h-screen relative flex">
+<div class="w-screen h-screen relative md:flex">
     <div class="w-full md:w-1/2 h-screen md:pl-6 md:py-6 md:pr-3 relative">
         <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
             <DataTable />
         </div>
     </div>
-    <div class="grid grid-cols-2 w-1/2">
+    <div class="md:grid h-full grid-rows-2 md:grid-cols-8 md:w-1/2 w-full">
         <!-- Top-Left -->
-        <div class="w-full md:px-3 md:pt-6 md:pb-3 relative">
-            <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
+        <div class="w-full md:col-span-5 md:px-3 md:pt-6 md:pb-3">
+            <div class="rounded-2xl flex justify-center items-center bg-base-300 h-full w-full">
+                <form class="w-11/12" use:enhance method="POST" action="?/addPassword">
+                    <input name="password" class="input w-full" placeholder="Password">
+                    <input name="maxuses" class="input w-full mt-2" placeholder="Max Uses" type="number" />
+                    <button type="submit" class="w-full btn btn-primary mt-2">
+                        Create Password
+                    </button>
+                </form>
             </div>
         </div>
         <!-- Top-Right -->
-        <div class="w-full md:pl-3 md:pr-6 md:pt-6 md:pb-3 relative">
+        <div class="w-full md:col-span-3 md:pl-3 md:pr-6 md:pt-6 md:pb-3">
             <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
             </div>
         </div>
         <!-- Bottom-Left -->
-        <div class="w-full md:px-3 md:pt-3 md:pb-6 relative">
+        <div class="w-full md:col-span-4 md:px-3 md:pt-3 md:pb-6 relative">
             <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
             </div>
         </div>
         <!-- Bottom-Right -->
-        <div class="w-full md:pl-3 md:pr-6 md:pt-3 md:pb-6 relative">
+        <div class="w-full md:col-span-4 md:pl-3 md:pr-6 md:pt-3 md:pb-6 relative">
             <div class="w-full h-full rounded-2xl bg-base-300 overflow-x-scroll">
             </div>
         </div>
