94431608cd0f5cce9b44030fd0b1c9c24f629290
April Hall <ari@arithefirst.com>
2024-10-27 14:57:16 -0400

    Fix formatting for on:click handlers

--- src/lib/alert.svelte
+++ src/lib/alert.svelte
@@ -14,24 +14,14 @@
   {#if alert.fail === true}
     <div role="alert" class="alert alert-error relative my-2" bind:this={self}>
       <p class="text-error-content">Failed to delete {alert.username}</p>
-      <button
-        class="absolute right-2 top-1/2 -translate-y-1/2"
-        on:click={() => {
-          self.remove();
-        }}
-      >
+      <button class="absolute right-2 top-1/2 -translate-y-1/2" on:click={() => self.remove()}>
         <Icon icon="mingcute:close-line" class="text-error-content" height="28" />
       </button>
     </div>
   {:else}
     <div role="alert" class="alert alert-success relative my-2" bind:this={self}>
       <p class="text-success-content">Successfully deleted {alert.username}</p>
-      <button
-        class="absolute right-2 top-1/2 -translate-y-1/2"
-        on:click={() => {
-          self.remove();
-        }}
-      >
+      <button class="absolute right-2 top-1/2 -translate-y-1/2" on:click={() => self.remove()}>
         <Icon icon="mingcute:close-line" class="text-success-content" height="28" />
       </button>
     </div>
@@ -40,24 +30,14 @@
   {#if alert.fail === true}
     <div role="alert" class="alert alert-error relative my-2" bind:this={self}>
       <p class="text-error-content">Failed to update {alert.username}</p>
-      <button
-        class="absolute right-2 top-1/2 -translate-y-1/2"
-        on:click={() => {
-          self.remove();
-        }}
-      >
+      <button class="absolute right-2 top-1/2 -translate-y-1/2" on:click={() => self.remove()}>
         <Icon icon="mingcute:close-line" class="text-error-content" height="28" />
       </button>
     </div>
   {:else}
     <div role="alert" class="alert alert-success relative my-2" bind:this={self}>
       <p class="text-success-content">Successfully updated {alert.username}</p>
-      <button
-        class="absolute right-2 top-1/2 -translate-y-1/2"
-        on:click={() => {
-          self.remove();
-        }}
-      >
+      <button class="absolute right-2 top-1/2 -translate-y-1/2" on:click={() => self.remove()}>
         <Icon icon="mingcute:close-line" class="text-success-content" height="28" />
       </button>
     </div>
